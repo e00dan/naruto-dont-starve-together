887a905f0c71ea9ab5c72175af733908f738ac43
Kuzirashi <kuzirashi@interia.pl>
2015-07-22 14:38:05 +0200

    Fixed bunshins holding invisible knifes

--- scripts/prefabs/bunshin.lua
+++ scripts/prefabs/bunshin.lua
@@ -4,6 +4,7 @@ local assets =
     Asset("SOUND", "sound/maxwell.fsb"),
     Asset("ANIM", "anim/swap_pickaxe.zip"),
     Asset("ANIM", "anim/swap_axe.zip"),
+    Asset("ANIM", "anim/kunai.zip"),
     Asset("ANIM", "anim/swap_kunai.zip")
 }
 
@@ -25,7 +26,7 @@ end
 
 local function EquipItem(inst, item)
     if item then
-        inst.AnimState:OverrideSymbol("swap_object", item, item)
+        inst.AnimState:OverrideSymbol("swap_object", 'swap_kunai', 'kunai')
         inst.AnimState:Show("ARM_carry") 
         inst.AnimState:Hide("ARM_normal")
     end
